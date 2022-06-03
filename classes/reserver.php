c7a2151514a47f1663ef41cb77309cc8c5caf32a
MAMADOU <mamadouwone2016@gmail.com>
2022-06-03 02:05:53 +0200

    new commit

--- classes/reserver.php
+++ classes/reserver.php
@@ -1,7 +1,7 @@
 <?php
 
-    class Reserver{
-
+    class Reserver
+    {
         private $numClient;
         private $numChambre;
         private $dateArrivee;
@@ -11,8 +11,8 @@
         // private $tel;
         // private $mail;
         
-        public function __construct($numClient = 0, $numChambre = 0, $dateArrivee = "", $dateDepart = "" ){
-
+        public function __construct($numClient = 0, $numChambre = 0, $dateArrivee = "", $dateDepart = "")
+        {
             $this->setNumClient($numClient);
             $this->setNumChambre($numChambre);
             $this->setDateArrivee($dateArrivee);
@@ -23,19 +23,43 @@
             // $this->setMail($mail);
         }
         
-        public function setNumClient($numClient){$this->numClient = $numClient;}
-        public function setNumChambre($numChambre){$this->numChambre = $numChambre;}
-        public function setDateArrivee($dateArrivee){$this->dateArrivee = $dateArrivee;}
-        public function setDateDepart($dateDepart){$this->dateDepart = $dateDepart;}
+        public function setNumClient($numClient)
+        {
+            $this->numClient = $numClient;
+        }
+        public function setNumChambre($numChambre)
+        {
+            $this->numChambre = $numChambre;
+        }
+        public function setDateArrivee($dateArrivee)
+        {
+            $this->dateArrivee = $dateArrivee;
+        }
+        public function setDateDepart($dateDepart)
+        {
+            $this->dateDepart = $dateDepart;
+        }
         // public function setNom($nom){$this->nom = $nom;}
         // public function setPrenom($prenom){$this->prenom = $prenom;}
         // public function setTel($tel){$this->tel = $tel;}
         // public function setMail($mail){$this->mail = $mail;}
 
-        public function getNumclient(){return $this->numClient;}
-        public function getNumChambre(){return $this->numChambre;}
-        public function getDateArrivee(){return $this->dateArrivee;}
-        public function getDateDepart(){return $this->dateDepart;}
+        public function getNumclient()
+        {
+            return $this->numClient;
+        }
+        public function getNumChambre()
+        {
+            return $this->numChambre;
+        }
+        public function getDateArrivee()
+        {
+            return $this->dateArrivee;
+        }
+        public function getDateDepart()
+        {
+            return $this->dateDepart;
+        }
         // public function getNom(){return $this->nom;}
         // public function getPrenom(){return $this->prenom;}
         // public function getTel(){return $this->tel;}
--- modele/ModeleUtilisateur.php
+++ modele/ModeleUtilisateur.php
@@ -1,24 +1,22 @@
 <?php
 
-class ModeleUtilisateur extends ModeleGenerique{
-
-   
-
-    public function connexion($login, $mot_de_passe ){
-        
+class ModeleUtilisateur extends ModeleGenerique
+{
+    public function connexion($login, $mot_de_passe)
+    {
         $stmt = $this->pdo->prepare("SELECT * FROM utilisateur WHERE login = ? AND mot_de_passe = ?");
         $stmt->execute([$login, $mot_de_passe]);
         
-        if( $stmt->rowcount() != 0 ){
+        if ($stmt->rowcount() != 0) {
             $resul = $stmt->fetch();
-            var_dump($resul);
+            // var_dump($resul);
             extract($resul);
-          $utilisateur = new Utilisateur($id_util, $login, "", $role);
+            $utilisateur = new Utilisateur($id_util, $login, "", $role);
             $_SESSION['utilisateur'] = serialize($utilisateur);
         }
     }
 
-    public function inscriptions(){
-
+    public function inscriptions()
+    {
     }
 }
